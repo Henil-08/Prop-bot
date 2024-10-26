7b973721cde0c089ed5272ba1f13be9bbc1fd50a
“Henil <henilgajjar08@gmail.com>
2024-10-26 03:00:57 -0400

    Final

--- src/main.py
+++ src/main.py
@@ -26,6 +26,8 @@ options.add_argument('--disable-gpu')
 options.add_argument('--headless')
 options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument("--window-size=1920x1080")
+options.add_argument("--no-sandbox")  # Required for some environments
+options.add_argument("--disable-dev-shm-usage")  # Reduce memory usage
 
 ## set up Streamlit 
 st.set_page_config(page_title="Prop-stream Bot", page_icon="🏢")
