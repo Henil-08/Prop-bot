67a76a8329d8a4318957676a7b722f63c17dee98
“Henil <henilgajjar08@gmail.com>
2024-10-26 02:05:42 -0400

    Final

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ def get_driver():
 options = Options()
 options.add_argument('--disable-gpu')
 options.add_argument('--headless')
+options.add_argument('--disable-blink-features=AutomationControlled')
 
 ## set up Streamlit 
 st.set_page_config(page_title="Prop-stream Bot", page_icon="🏢")
