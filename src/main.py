af9be6030235750a120e6af70cf1a82e5eaa5738
“Henil <henilgajjar08@gmail.com>
2024-10-26 02:49:38 -0400

    remove proceed

--- src/main.py
+++ src/main.py
@@ -43,37 +43,37 @@ if city and option:
     login.login_to_propstream(USERNAME, PASSWORD, driver)
     st.success("Login Successful")
 
-    try:
-        proceed_xpath = '//*[@id="root"]/div/div[2]/div/div/div[2]/div/div[3]/button[2]/div[1]'
-        proceed_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, proceed_xpath))
-        )
-        proceed_element.click()
-        print("Element found and clicked.")
-    except TimeoutException:
-        print("Element not found, proceeding with the alternative task.")
-        login.search_city(city, driver)
+    # try:
+    #     proceed_xpath = '//*[@id="root"]/div/div[2]/div/div/div[2]/div/div[3]/button[2]/div[1]'
+    #     proceed_element = WebDriverWait(driver, 10).until(
+    #         EC.presence_of_element_located((By.XPATH, proceed_xpath))
+    #     )
+    #     proceed_element.click()
+    #     print("Element found and clicked.")
+    # except TimeoutException:
+    #     print("Element not found, proceeding with the alternative task.")
+    login.search_city(city, driver)
 
-        # Select Filters
-        filters.click_filter_button(driver)
-        st.success("Filter Button Clicked")
-        filters.select_filter_option(driver, False) if option == 'Standard' else filters.select_filter_option(driver, True)
-        st.success("Filters Selected")
+    # Select Filters
+    filters.click_filter_button(driver)
+    st.success("Filter Button Clicked")
+    filters.select_filter_option(driver, False) if option == 'Standard' else filters.select_filter_option(driver, True)
+    st.success("Filters Selected")
 
-        # Get the total number of listings
-        total_listings = utility.get_total_listings(driver)
-        listings_per_page = 50  # Assuming 50 listings per page
-        total_pages = math.ceil(total_listings / listings_per_page)  # Calculate total pages
-        
-        all_listings_data = extract.get_search_data(driver, total_pages, total_listings)
-        st.success("Search Complete")
-        
-        csv = utility.save_to_csv(all_listings_data)
+    # Get the total number of listings
+    total_listings = utility.get_total_listings(driver)
+    listings_per_page = 50  # Assuming 50 listings per page
+    total_pages = math.ceil(total_listings / listings_per_page)  # Calculate total pages
+    
+    all_listings_data = extract.get_search_data(driver, total_pages, total_listings)
+    st.success("Search Complete")
+    
+    csv = utility.save_to_csv(all_listings_data)
 
-        st.download_button(
-            "Press to Download",
-            csv,
-            f"{city}_{option}.csv",
-            "text/csv",
-            key='download-csv'
-        )
+    st.download_button(
+        "Press to Download",
+        csv,
+        f"{city}_{option}.csv",
+        "text/csv",
+        key='download-csv'
+    )
